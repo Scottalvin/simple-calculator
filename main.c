d642a7cb7a9f7e1095d3d84b2003d3babed0f325
Scottalvin <106369202+Scottalvin@users.noreply.github.com>
2022-07-01 11:24:15 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+Simple calculator
+created by alvin scott
+on july,2022
+c89 compiler
+MIT license
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int num1,num2,sum,diff,product;
+float quotient;
+    printf("\Simple calculator!\n");
+    printf ("Enter two numbers:");
+    scanf("%d%",num1,&num2);
+    //computations
+    sum = num + num2;
+    diff = num1 -num2;
+    product = num1 * num2;
+    quotient = (float)num1/num2;
+    //output
+    printf("%d+%d=%d\n",num1,num2,sum);
+    printf("%d-%d=%d\n",num1,num2,diff);
+    printf("%d*%d=%d\n",num1,num2,product);
+    printf("%d/%d=%f\n",num1,num2,quotient);
+    return 0;
+}
